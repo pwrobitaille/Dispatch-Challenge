9fe9cdf865c6bcc11102d45373a7445623b13299
Peter Robitaille <peterrobitaille@Peters-MacBook-Pro.local>
2017-11-22 10:38:22 -0500

    got google api to return restaurants

--- ruby.rb
+++ ruby.rb
@@ -1,1 +1,17 @@
 require 'httparty'
+require 'geocoder'
+require 'pry'
+
+puts "Please enter an address"
+user_address = gets.chomp
+address = URI.escape(user_address)
+
+
+google_url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=reataurants+in+#{address}&radius=10000&key=AIzaSyCakzloOI6TYj9uwBVWVleB9fF123zicws"
+response = HTTParty.get(google_url)
+results = response.parsed_response["results"]
+
+puts "20 Restaurants near #{user_address}"
+results.each do |result|
+  puts result["name"]
+end
